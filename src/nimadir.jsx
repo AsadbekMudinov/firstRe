b4ff141436ec6efb00a8f4dd690c9a3de2e3cb15
Asadbek <Asadbek@gmail.com>
2021-11-23 22:49:23 +0500

    nimadirda menyu yaratdim

--- src/nimadir.jsx
+++ src/nimadir.jsx
@@ -4,6 +4,12 @@ const Nimadir = () => {
     return (
         <div>
             <h1>Salom</h1>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">Contacts</a></li>
+            </ul>
         </div>
     )
 }
