04f0b4096156c1963ebdda8a1121e87ed24c44c5
Asadbek <Asadbek@gmail.com>
2021-11-23 22:31:04 +0500

    birinchi kommentim

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import React from 'react'
+import About from './About'
+import {BrowserRouter as Router, Routes, Link, Route} from 'react-router-dom'
+import Home from './Home'
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App" >
+      <h1>Hello</h1>
+      {/* <Router>
+        <Link to='home'>Home</Link>
+        <Link to='about'>About</Link>
+        <Routes>
+          <Route path='home' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='/user/:username' element={<User />} />
+        </Routes>
+      </Router> */}
+      <Home />
+      
+    </div>
+  );
+}
+
+export default App;
